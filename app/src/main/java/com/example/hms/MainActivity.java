f338a7d77e2614bfb1e6c08072e78f63ba9018de
Mohsin Shabbir <shabbirmohsin001@gmail.com>
2022-11-25 01:27:26 +0500

    User Login changes

--- app/src/main/java/com/example/hms/MainActivity.java
+++ app/src/main/java/com/example/hms/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hms;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,12 +11,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 EditText edt_Username,edt_pass;
 Button btn_user,btn_admin;
 TextView register;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +33,13 @@ TextView register;
         btn_user = findViewById(R.id.user_btn);
         btn_admin=findViewById(R.id.admin_btn);
         register = (TextView) findViewById(R.id.newAccount);
+        mAuth = FirebaseAuth.getInstance();
 
 
         btn_user.setOnClickListener(this);
 
         register.setOnClickListener(this);
+
 
 
 
@@ -46,8 +56,35 @@ TextView register;
                 startActivity(new Intent(this,RegisterActivity.class));
                 break;
             case R.id.user_btn:
-                startActivity(new Intent(this,UserPanelActivity.class));
+                login();
         }
 
     }
+
+    private void login() {
+        String user=edt_Username.getText().toString().trim();
+        String pass=edt_pass.getText().toString().trim();
+        if (user.isEmpty()){
+            edt_Username.setError("Email is Required");
+        }if (pass.isEmpty()){
+            edt_pass.setError("Password is Required");
+        }
+        else {
+            mAuth.signInWithEmailAndPassword(user,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MainActivity.this, UserPanelActivity.class));
+
+                    }else {
+                        Toast.makeText(MainActivity.this, "Login Failed"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
+
+    }
+
 }
