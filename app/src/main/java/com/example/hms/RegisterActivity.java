62b76bb459f70ec17aae5e187850fefaa29a96c0
Talha Jabbar <talhajabbr52@gmail.com>
2022-11-27 19:19:19 +0500

    Gender shifted to Radio button

--- app/src/main/java/com/example/hms/RegisterActivity.java
+++ app/src/main/java/com/example/hms/RegisterActivity.java
@@ -4,7 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -21,16 +21,15 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.Locale;
-import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
     private FirebaseAuth mAuth;
-    private EditText name,email,pass,age,fName,address,phone,gender;
+    private EditText name, email, pass, age, fName, address, phone;
     private Button register;
-    private RadioButton male,female;
+    private RadioButton male, female;
     private TextView banner;
-    String userGender="";
+    String userGender = "";
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,27 +37,28 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         setContentView(R.layout.activity_register);
         mAuth = FirebaseAuth.getInstance();
         //Assigning objet their ID's
-        name = (EditText) findViewById(R.id.txtUser);
-        fName = (EditText) findViewById(R.id.txtFName);
-        email = (EditText) findViewById(R.id.txtEmail);
-        pass = (EditText) findViewById(R.id.txtPass);
-        phone = (EditText) findViewById(R.id.txtPhone);
-        age = (EditText) findViewById(R.id.txtAge);
-        address = (EditText) findViewById(R.id.txtAddress);
-        banner = (TextView) findViewById(R.id.signIn);
-        register = (Button) findViewById(R.id.Register_btn);
-        //gender=(EditText) findViewById(R.id.gender);
-        male = (RadioButton) findViewById(R.id.male);
-        female = (RadioButton) findViewById(R.id.female);
+        name = findViewById(R.id.txtUser);
+        fName = findViewById(R.id.txtFName);
+        email = findViewById(R.id.txtEmail);
+        pass = findViewById(R.id.txtPass);
+        phone = findViewById(R.id.txtPhone);
+        age = findViewById(R.id.txtAge);
+        address = findViewById(R.id.txtAddress);
+        banner = findViewById(R.id.signIn);
+        register = findViewById(R.id.Register_btn);
+
+        male = findViewById(R.id.male);
+        female = findViewById(R.id.female);
 
         //function implements
-        
+
         banner.setOnClickListener(this);
         register.setOnClickListener(this);
 
 
-
     }
+
+    @SuppressLint("NonConstantResourceId")
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
@@ -68,7 +68,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             case R.id.male:
                 if (checked) {
                     female.setChecked(false);
-                    userGender="Male";
+                    userGender = "Male";
 
                     break;
                 } else {
@@ -78,19 +78,20 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             case R.id.female:
                 if (checked) {
                     male.setChecked(false);
-                    userGender="female";
+                    userGender = "female";
                     break;
                 } else {
                     female.setError("Select Gender");
                 }
         }
     }
+
     @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.signIn:
-                startActivity(new Intent(this,MainActivity.class));
+                startActivity(new Intent(this, MainActivity.class));
                 break;
             case R.id.Register_btn:
                 registerUser();
@@ -101,97 +102,88 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
     private void registerUser() {
 
-        String Name,FName,Email,Pass,Phone,Address,Age, usergender;
+        String Name, FName, Email, Pass, Phone, Address, Age, uGender;
         Name = name.getText().toString().trim();
         FName = fName.getText().toString().trim();
         Email = email.getText().toString().trim();
         Age = age.getText().toString().trim();
         Pass = pass.getText().toString().trim();
-        usergender=userGender.toString().trim();
+        uGender = userGender;
         Address = address.getText().toString().trim();
         Phone = phone.getText().toString().trim();
 
 
-        if (Name.isEmpty())
-        {
+        if (Name.isEmpty()) {
             name.setError("Enter Full Name");
             name.requestFocus();
 
         }
-        if (FName.isEmpty())
-        {
+        if (FName.isEmpty()) {
             fName.setError("Enter Full Name");
             fName.requestFocus();
 
         }
-        if (Email.isEmpty())
-        {
+        if (Email.isEmpty()) {
             email.setError("Email required");
             email.requestFocus();
 
         }
-        if (!Patterns.EMAIL_ADDRESS.matcher(Email).matches()){
+        if (!Patterns.EMAIL_ADDRESS.matcher(Email).matches()) {
             email.setError("Enter valid Email");
             email.requestFocus();
         }
 
-        if (Pass.isEmpty())
-        {
+        if (Pass.isEmpty()) {
             pass.setError("Enter Full Name");
             name.requestFocus();
 
         }
-        if (pass.length()<=8)
-        {
+        if (pass.length() <= 8) {
             pass.setError("Password must be grater than 8 character");
             pass.requestFocus();
         }
 
-        if (Address.isEmpty())
-        {
+        if (Address.isEmpty()) {
             address.setError("Address required");
             address.requestFocus();
 
         }
-        if (Age.isEmpty())
-        {
+        if (Age.isEmpty()) {
             age.setError("Age required");
             age.requestFocus();
 
         }
 
-        if (Phone.isEmpty())
-        {
+        if (Phone.isEmpty()) {
             phone.setError("Phone no required");
             phone.requestFocus();
 
         }
 
-        mAuth.createUserWithEmailAndPassword(Email,Pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        mAuth.createUserWithEmailAndPassword(Email, Pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()){
+                if (task.isSuccessful()) {
 
-                   UserRegistration user = new UserRegistration(Name,FName,Email,Phone,Age,usergender,Address);
+                    UserRegistration user = new UserRegistration(Name, FName, Email, Phone, Age, uGender, Address);
 
                     FirebaseDatabase.getInstance().getReference("User Registration").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()){
+                            if (task.isSuccessful()) {
                                 Toast.makeText(RegisterActivity.this, "Registration Successful", Toast.LENGTH_LONG).show();
-                                 startActivity(new Intent(RegisterActivity.this,MainActivity.class));
+                                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
 
 
-                            }
-                            else {
+                            } else {
                                 Toast.makeText(RegisterActivity.this, "Registration Failed", Toast.LENGTH_LONG).show();
 
                             }
                         }
                     });
 
-                }
-                else {
+                } else {
+
                     Toast.makeText(RegisterActivity.this, "Registration Failed ", Toast.LENGTH_LONG).show();
 
                 }
@@ -199,18 +191,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         });
 
 
-
-
-
-
-
-
-
-
-
-
     }
 
 
-
-    }
+}
