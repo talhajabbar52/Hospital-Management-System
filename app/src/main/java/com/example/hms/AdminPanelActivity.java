72a60d4ef84f641a660b516b2044a4936eef8bf0
Dell <saadurrehman206@gmail.com>
2022-12-14 20:03:03 +0500

    adminpanel

---
+++ app/src/main/java/com/example/hms/AdminPanelActivity.java
@@ -0,0 +1,65 @@
+package com.example.hms;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class AdminPanelActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_panel);
+
+        Toolbar Admin_toolbar = findViewById(R.id.Admin_toolbar);
+        setSupportActionBar(Admin_toolbar);
+
+        drawer = findViewById(R.id.admin_drawer_layout);
+        NavigationView navigationView = findViewById(R.id.admin_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, Admin_toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.Admin_fragment_container, new AddDoctorsFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_add_doctors);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_add_doctors:
+                getSupportFragmentManager().beginTransaction().replace(R.id.Admin_fragment_container, new AddDoctorsFragment()).commit();
+                break;
+
+            case R.id.nav_appointment_check:
+                getSupportFragmentManager().beginTransaction().replace(R.id.Admin_fragment_container, new AppointmentCheckFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+}
